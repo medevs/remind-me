0863ae74ed64aee5a3871220680c51ee9471c993
ahmed <oublihi.a@gmail.com>
2023-09-22 12:13:56 +0200

    starting the dashboard

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "RemindMe",
-  description: "Created by: @CodeWithKliton",
+  description: "RemindMe app",
 };
 
 export default function RootLayout({
