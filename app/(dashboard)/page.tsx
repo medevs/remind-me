6f89a2b87bc1e809773a628dcac80c48f58717ae
ahmed <oublihi.a@gmail.com>
2023-09-23 17:15:49 +0200

    setup prisma & create collections

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -2,6 +2,12 @@ import { Skeleton } from "@/components/ui/skeleton";
 import { currentUser } from "@clerk/nextjs";
 import { Suspense } from "react";
 
+import { prisma } from "@/lib/prisma";
+// import CollectionCard from "@/components/CollectionCard";
+import CreateCollectionBtn from "@/components/CreateCollectionBtn";
+import SadFace from "@/components/icons/SadFace";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+
 export default async function Home() {
   return (
     <>
@@ -9,6 +15,7 @@ export default async function Home() {
         <WelcomMsg />
       </Suspense>
       <Suspense fallback={<div>Loading collections...</div>}>
+        <CollectionList />
       </Suspense>
     </>
   );
@@ -38,5 +45,44 @@ function WelcomeMsgFallback() {
         <Skeleton className="w-[150px] h-[36px]" />
       </h1>
     </div>
+  );
+}
+
+async function CollectionList() {
+  const user = await currentUser();
+  const collections = await prisma.collection.findMany({
+    // include: {
+    //   tasks: true,
+    // },
+    where: {
+      userId: user?.id,
+    },
+  });
+
+  if (collections.length === 0) {
+    return (
+      <div className="flex flex-col gap-5">
+        <Alert>
+          <SadFace />
+          <AlertTitle>There are no collections yet!</AlertTitle>
+          <AlertDescription>
+            Create a collection to get started
+          </AlertDescription>
+        </Alert>
+        <CreateCollectionBtn />
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <CreateCollectionBtn />
+      <div className="flex flex-col gap-4 mt-6">
+        {collections.map((collection) => (
+          // <CollectionCard key={collection.id} collection={collection} />
+          collection.id
+        ))}
+      </div>
+    </>
   );
 }
